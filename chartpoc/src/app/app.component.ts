ca10959e57d0e38acef3568c7e32f79dd98da71c
Himanshu <kontacthimanshu@hotmail.com>
2022-07-27 17:55:51 +0530

    added code to render rows

--- chartpoc/src/app/app.component.ts
+++ chartpoc/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chartpoc';
+  burst:boolean = false;
+  oi:number = 3209878;
+
+  changeValue()
+  {
+    this.oi = this.oi + 10000;
+    this.burst = true;
+  }
+
 }
